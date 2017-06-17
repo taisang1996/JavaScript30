575a357da7d8a75d2594f0c7ae519e55b22e2419
Nguyen Tan Tai <taisang1996@gmail.com>
2017-06-17 19:12:45 +0700

    19 - done - khó quá

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,70 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(mediaStream => {
+      video.src = window.URL.createObjectURL(mediaStream)
+      video.play()
+    })
+    .catch(console.error)
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth
+  const height = video.videoHeight
+
+  canvas.width = width
+  canvas.height = height
+
+  setInterval(() => {
+    ctx.drawImage(video, 0,0, width, height)
+    let pixels = ctx.getImageData(0, 0, width, height)
+    pixels = greenScreen(pixels)
+    ctx.putImageData(pixels, 0, 0)
+  }, 16)
+}
+
+function takePhoto() {
+  //play sound
+  snap.currentTime = 0
+  snap.play() 
+
+  //take the data out of canvas
+  const data = canvas.toDataURL('image/jpeg')
+  
+  const link = document.createElement('a')
+  link.href = data 
+  link.setAttribute('download', 'webcam')
+  link.textContent = 'Download image'
+  link.innerHTML = `<img src="${data}" />`
+  strip.insertBefore(link, strip.firstChild)
+}
+
+function greenScreen(pixels) {
+  const levels = {}
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value
+  })
+
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    let red = pixels.data[i + 0]
+    let green = pixels.data[i + 1]
+    let blue = pixels.data[i + 2]
+    let alpha = pixels.data[i + 3]
+    
+    if (red >= levels.rmin && red <= levels.rmax
+     && green >= levels.gmin && green <= levels.gmax
+     && blue >= levels.bmin && blue <= levels.bmax
+    ) {
+      pixels.data[i + 3] = 0
+    }
+  }
+
+  return pixels
+}
+
+getVideo()
+
+video.addEventListener('canplay', paintToCanvas)
